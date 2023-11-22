cac0d4438ea49213f49847ce8d3eceeb19a72c0a
Genie <devgeniegenie@gmail.com>
2023-11-22 21:25:14 +0900

    user-service - Routes 변경, 오류 변경(WebSecurity 로직 변경)

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,40 +1,26 @@
 package com.example.userservice.security;
 
-import com.example.userservice.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-/*@Configuration
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
-    }
-
-}*/
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
     private AuthenticationManager authenticationManager;
-    private UserService userService;
-    private PasswordEncoder bCryptPasswordEncoder;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("222.108.100.32")
+        http.authorizeRequests()
+                .antMatchers("/error/**").permitAll()
+                .antMatchers("/**")
+                .hasIpAddress("220.118.172.90")
                 .and()
-                .addFilter(getAuthenticationFilter(authenticationManager));
+                .addFilter(getAuthenticationFilter(authenticationManager))
+        ;
 
         return http.build();
     }
@@ -43,9 +29,5 @@ public class WebSecurity {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter();
         authenticationFilter.setAuthenticationManager(authenticationManager);
         return authenticationFilter;
-    }
-    
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
